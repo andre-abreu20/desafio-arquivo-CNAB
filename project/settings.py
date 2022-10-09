addc49dc437cda29deeed79c0f7fe625d24d067b
Andre Luis <andre.luismesquita@hotmail.com>
2022-10-09 17:23:02 -0300

    feat: upload content is avaible in Form CNAB

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "web_challenge.apps.WebChallengeConfig",
+    "web_challenge",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FileUploadParser"
+    ]
+}
